bf8ce7d578992d102f0dc1ac8ff3ed972ae37b1b
Mike (stew) O'Connor <stew@vireo.org>
2018-04-25 20:34:18 -0700

    lendup

---
+++ stew/stew.sh
@@ -0,0 +1,21 @@
+# Personal Info
+PHONE_NUMBER="408.466.5060"
+URL=""
+EMAIL="mikeyo@gmail.com"
+ADDRESS1="stew.vireo.org"
+ADDRESS2="github.com/stew"
+REPOS=""
+SUBTITLE1="Software Architect"
+
+# Local Build Config - provide absolute paths to override DEFAULT_FILES
+PERSONAL_INFO_TEMPLATES=    # define blocks for .org, .html, etc...
+HTML_BASE_TEMPLATE=         # pandoc HTML5 template
+ODT_BASE_TEMPLATE=          # pandoc ODT template
+ODT_REFERENCE=              # pandoct ODT reference file (for styles)
+CSS_LOCAL=                  # local css stylesheet will be uploaded on publish
+
+# Remote Sync Config - no SSH key? provide password or .pem file path
+CSS_REMOTE= #"http://example.com/static/resume/resume.css"
+WEBSERVER= #"devguy@example.com:webapps/staticapp/resume"
+CRED_PEM=
+CRED_PASS=
